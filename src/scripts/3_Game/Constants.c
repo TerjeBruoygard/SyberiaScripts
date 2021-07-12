08753b71829e3585f3e0bc2ae0c53d9cb901e4d0
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-07-12 10:09:21 +0300

    Colorize icons

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,6 +1,6 @@
 const string Syberia_ModPreffix = "[SYBERIA] ";
 const bool Syberia_DebugMode = false;
-const string Syberia_Version = "OPEN BETA 0.47f";
+const string Syberia_Version = "OPEN BETA 0.47g";
 
 void SybLog(string message)
 {
