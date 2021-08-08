373f3c53284bb3c9457a00f80921f1389e3bd2bf
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-08-08 17:32:13 +0300

    Add logo files and update the version

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,6 +1,6 @@
 const string Syberia_ModPreffix = "[SYBERIA] ";
 const bool Syberia_DebugMode = false;
-const string Syberia_Version = "OPEN BETA 0.48f";
+const string Syberia_Version = "OPEN BETA 0.48g";
 
 void SybLog(string message)
 {
