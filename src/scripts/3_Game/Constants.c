fd3d8ae65a2d45658de05e8bccd09ef4ac330437
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-08-15 15:42:12 +0300

    Next step to implement barter

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,6 +1,6 @@
 const string Syberia_ModPreffix = "[SYBERIA] ";
-const bool Syberia_DebugMode = false;
-const string Syberia_Version = "OPEN BETA 0.48g";
+const bool Syberia_DebugMode = true;
+const string Syberia_Version = "OPEN BETA 0.48h";
 
 void SybLog(string message)
 {
