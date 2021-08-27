df6ebc02cc107e54d7138b09f72df81f2b5a280a
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-08-27 18:16:44 +0300

    Fix trader issues and improve safe zone

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,6 +1,6 @@
 const string Syberia_ModPreffix = "[SYBERIA] ";
 const bool Syberia_DebugMode = false;
-const string Syberia_Version = "OPEN BETA 0.48i";
+const string Syberia_Version = "OPEN BETA 0.48j";
 
 void SybLog(string message)
 {
