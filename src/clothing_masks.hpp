4d4d708e8e1147774ee8f2f3ee64e80e7d21a31d
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-09-22 18:12:03 +0300

    Fix gasmasks adter update 1.14

--- src/clothing_masks.hpp
+++ src/clothing_masks.hpp
@@ -1,47 +1,9 @@
-class GP5GasMask: Clothing
+class GasMask: Clothing
 {
-    hiddenSelectionsTextures[]=
-    {
-        "SyberiaScripts\data\masks\gp5gasmask_white_ca.paa",
-        "SyberiaScripts\data\masks\gp5gasmask_white_ca.paa",
-        "SyberiaScripts\data\masks\gp5gasmask_white_ca.paa",
-        
-        "\dz\characters\masks\data\GP5GasMask_white_co.paa",
-        "\dz\characters\masks\data\GP5GasMask_white_co.paa",
-        "\dz\characters\masks\data\GP5GasMask_white_co.paa"
-    };
-    attachments[]=
-    {
-        "GasMaskFilter"
-    };
-};
-
-class AirborneMask: Clothing
-{
-    hiddenSelectionsTextures[]=
-    {
-        "\dz\characters\masks\data\airborne_mask_co.paa",
-        "\dz\characters\masks\data\airborne_mask_co.paa",
-        "\dz\characters\masks\data\airborne_mask_co.paa",
-        "",
-        "\dz\characters\masks\data\airborne_mask_glass_ca.paa",
-        "\dz\characters\masks\data\gp5gasmask_white_co.paa",
-    };
-    attachments[]=
-    {
-        "GasMaskFilter"
-    };
+    radiationProtection = 0.29;
 };
 
 class GP5GasMask_Filter: Inventory_Base
 {
-    displayName="#syb_gp5_filter_name";
-    descriptionShort="#syb_gp5_filter_desc";
-    stackedUnit="percentage";
-    quantityBar=1;
-    varQuantityInit=100;
-    varQuantityMin=0;
-    varQuantityMax=100;
-    varQuantityDestroyOnMin=0;
     radiationProtection = 0.29;
 };
