2966509b2134b1ef6458f211fdf9316ac1cb05a4
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-09-29 15:46:11 +0300

    Update version

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,6 +1,6 @@
 const string Syberia_ModPreffix = "[SYBERIA] ";
 const bool Syberia_DebugMode = false;
-const string Syberia_Version = "OPEN BETA 0.48L";
+const string Syberia_Version = "OPEN BETA 0.49";
 
 void SybLog(string message)
 {
