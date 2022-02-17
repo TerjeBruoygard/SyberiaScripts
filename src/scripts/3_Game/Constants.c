834380ca5d0a5f24e6fef92fdef651c9010f93f4
ivan.kishchenko <ivan.kishchenko@aspose.com>
2022-02-17 17:06:33 +0300

    Fix animals as items skinning

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,9 +1,9 @@
 const string Syberia_ModPreffix = "[SYBERIA] ";
 const bool Syberia_DebugMode = false;
-const string Syberia_Version = "OPEN BETA 0.49L";
+const string Syberia_Version = "OPEN BETA 0.49M";
 
 class SyberiaVersionChecker {
-	void CLIENT_AND_SERVER_MODS_VERSION_MISSMATCH_0_49L() {}
+	void CLIENT_AND_SERVER_MODS_VERSION_MISSMATCH_0_49M() {}
 }
 
 void SybLog(string message)
--- src/scripts/4_World/Entities/Creatures/Animals/Animal_GallusGallusDomesticus.c
+++
@@ -1,7 +0,0 @@
-modded class Animal_GallusGallusDomesticus
-{
-    override bool CanBeSkinned()
-    {
-        return true;
-    }
-};
