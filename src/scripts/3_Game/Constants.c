64bcc6dcde269a13439f61583cbdb12cad487611
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-05-17 20:45:58 +0300

    Update Constants.c
    Version 0.49K

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,9 +1,9 @@
 const string Syberia_ModPreffix = "[SYBERIA] ";
 const bool Syberia_DebugMode = false;
-const string Syberia_Version = "OPEN BETA 0.49M";
+const string Syberia_Version = "OPEN BETA 0.49K";
 
 class SyberiaVersionChecker {
-	void CLIENT_AND_SERVER_MODS_VERSION_MISSMATCH_0_49M() {}
+	void CLIENT_AND_SERVER_MODS_VERSION_MISSMATCH_0_49K() {}
 }
 
 void SybLog(string message)
