65fe3089866961b306fc978d4ef2bc680d9b4f63
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-06-01 20:45:49 +0300

    Update ModItemRegisterCallbacks.c

--- src/scripts/4_World/Entities/ManBase/DayZPlayer/ModItemRegisterCallbacks.c
+++ src/scripts/4_World/Entities/ManBase/DayZPlayer/ModItemRegisterCallbacks.c
@@ -21,6 +21,8 @@ modded class ModItemRegisterCallbacks
 	
 	override void RegisterTwoHanded(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
 	{
+		super.RegisterTwoHanded(pType, pBehavior);
+	
 		pType.AddItemInHandsProfileIK("SleepingBag_BaseColor", "dz/anims/workspaces/player/player_main/weapons/player_main_2h_extinguisher.asi", pBehavior, "dz/anims/anm/player/ik/two_handed/WaterproofBag.anm");	
 	}
 };
