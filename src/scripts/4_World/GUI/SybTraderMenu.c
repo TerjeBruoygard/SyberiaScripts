b58785afe623bd1805ab2895809abf36a9807b4e
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:38:43 +0400

    Next step to implement traider

--- src/scripts/4_World/GUI/SybTraderMenu.c
+++ src/scripts/4_World/GUI/SybTraderMenu.c
@@ -21,6 +21,7 @@ class SybTraderMenu extends UIScriptedMenu
 	
 	ref array<ref Widget> m_sellWidgetsCache = new array<ref Widget>;
 	ref array<ref Widget> m_buyWidgetsCache = new array<ref Widget>;
+	ref array<ref SybTraderMenu_BuyData> m_buyData = new array<ref SybTraderMenu_BuyData>;
 	ref array<EntityAI> m_previewItemsCache = new array<EntityAI>;
 	
 	void InitMetadata(int traderId, ref PluginTrader_Trader traderInfo, ref PluginTrader_Data traderData)
@@ -44,6 +45,12 @@ class SybTraderMenu extends UIScriptedMenu
 	
 	void CleanupUI()
 	{
+		foreach (ref SybTraderMenu_BuyData buyData : m_buyData)
+		{
+			delete buyData;
+		}
+		m_buyData.Clear();
+		
 		foreach (ref Widget w1 : m_sellWidgetsCache)
 		{
 			w1.Unlink();
@@ -176,9 +183,15 @@ class SybTraderMenu extends UIScriptedMenu
 		itemBuy.GetSize(w, h);
 		itemBuy.SetPos(0, (h + SELL_ITEM_HEIGHT_OFFSET) * index);
 		itemBuy.SetSize(contentWidth, h);
+		itemBuy.SetUserID(index);
+		
+		ref SybTraderMenu_BuyData actionBtnParam = new ref SybTraderMenu_BuyData;
+		actionBtnParam.m_classname = classname;
+		actionBtnParam.m_totalQuantity = quantity;
+		actionBtnParam.m_maxBuyQuantity = pluginTrader.CalculateBuyMaxQuantity(m_traderInfo, classname);
+		actionBtnParam.m_selectedQuantity = Math.Clamp(quantity, 1, actionBtnParam.m_maxBuyQuantity);
 		
 		ref ButtonWidget actionButton = ButtonWidget.Cast( itemBuy.FindAnyWidget( "ItemActionButton" ) );
-		actionButton.SetUserData(new Param2<string, float>(classname, quantity));
 		actionButton.SetUserID(2001);
 		
 		ref ItemPreviewWidget previewWidget = ItemPreviewWidget.Cast( itemBuy.FindAnyWidget( "ItemPreviewWidget" ) );		
@@ -190,15 +203,23 @@ class SybTraderMenu extends UIScriptedMenu
 		WidgetSetWidth(itemBuy, "ItemNameWidget", contentWidth - 220);
 		WidgetTrySetText(itemBuy, "ItemNameWidget", item.GetDisplayName());
 		WidgetTrySetText(itemBuy, "ItemPriceWidget", pluginTrader.CalculateBuyPrice(m_traderInfo, classname, quantity).ToString());		
-		UpdateItemInfoQuantity(itemBuy, pluginTrader, classname, quantity);			
+		UpdateItemInfoQuantity(itemBuy, pluginTrader, classname, quantity);					
+		UpdateItemInfoSelectedQuantity(itemBuy, classname, actionBtnParam.m_selectedQuantity, actionBtnParam.m_maxBuyQuantity);
+		
+		ref ButtonWidget minusButton = ButtonWidget.Cast( itemBuy.FindAnyWidget( "MinusActionBtn" ) );
+		minusButton.SetUserID(3001);
+		
+		ref ButtonWidget plusButton = ButtonWidget.Cast( itemBuy.FindAnyWidget( "PlusActionBtn" ) );
+		plusButton.SetUserID(3002);
 		
 		m_buyWidgetsCache.Insert(itemBuy);
+		m_buyData.Insert(actionBtnParam);
 		return index;
 	}
 	
 	string FormatBuyQuantityStr(float quantity)
 	{
-		int quantityInt = (int)(quantity * 10.0);
+		int quantityInt = (int)Math.Round(quantity * 10.0);
 		string quantityStr = quantityInt.ToString();
 		if (quantityInt % 10 != 0)
 		{
@@ -343,6 +364,7 @@ class SybTraderMenu extends UIScriptedMenu
 			delete m_traderData;
 		}
 
+		delete m_buyData;
 		delete m_sellWidgetsCache;
 		delete m_buyWidgetsCache;
 				
@@ -432,16 +454,48 @@ class SybTraderMenu extends UIScriptedMenu
 			if (enable)
 			{
 				back.SetColor(ARGB(200, 16, 87, 20));
-				btn.SetUserID(1002);
+				btn.SetUserID(2002);
 			}
 			else
 			{
 				back.SetColor(ARGB(200, 25, 25, 25));
-				btn.SetUserID(1001);
+				btn.SetUserID(2001);
 			}
 		}
 		
 		UpdateCurrentPriceProgress();
+	}
+	
+	private void ChangeBuyQuantity(ref ButtonWidget btn, float value)
+	{
+		PluginTrader pluginTrader = PluginTrader.Cast(GetPlugin(PluginTrader));
+		if (!pluginTrader)
+			return;
+		
+		ref Widget mainWidget = btn.GetParent().GetParent().GetParent().GetParent();
+		int id = mainWidget.GetUserID();
+		
+		ref ButtonWidget mainButton = ButtonWidget.Cast( mainWidget.FindAnyWidget("ItemActionButton") );
+		SelectBuyItem(mainButton, true);
+				
+		ref SybTraderMenu_BuyData mainParam = m_buyData.Get(id);	
+		
+		float stepSize = pluginTrader.CalculateItemSelectedQuantityStep(mainParam.m_classname);
+		if (value < 0 && mainParam.m_selectedQuantity <= 1)
+		{
+			value = stepSize * -1;
+		}
+		else if (value > 0 && mainParam.m_selectedQuantity < 1)
+		{
+			value = stepSize;
+		}
+			
+		if ((mainParam.m_selectedQuantity + value) > 0 && (mainParam.m_selectedQuantity + value) <= mainParam.m_maxBuyQuantity)
+		{
+			mainParam.m_selectedQuantity = mainParam.m_selectedQuantity + value;
+			mainParam.m_selectedQuantity = Math.Min(mainParam.m_selectedQuantity, mainParam.m_maxBuyQuantity);
+			UpdateItemInfoSelectedQuantity(mainWidget, mainParam.m_classname, mainParam.m_selectedQuantity, mainParam.m_maxBuyQuantity);
+		}
 	}
 	
 	private void DoBarter()
@@ -477,6 +531,14 @@ class SybTraderMenu extends UIScriptedMenu
 			else if (w.GetUserID() == 2002) // buy item unselect
 			{
 				SelectBuyItem(ButtonWidget.Cast(w), false);
+			}
+			else if (w.GetUserID() == 3001) // minus buy quantity
+			{
+				ChangeBuyQuantity(ButtonWidget.Cast(w), -1);
+			}
+			else if (w.GetUserID() == 3002) // plus buy quantity
+			{
+				ChangeBuyQuantity(ButtonWidget.Cast(w), 1);
 			}
 			else if (w == m_barterBtn)
 			{
@@ -642,6 +704,63 @@ class SybTraderMenu extends UIScriptedMenu
 		}
 	}
 	
+	private void UpdateItemInfoSelectedQuantity(Widget root_widget, string classname, float quantity, float maxQuantity)
+	{		
+		int maxStackSize = 0;		
+		if (GetGame().ConfigIsExisting(CFG_VEHICLESPATH + " " + classname))
+		{
+			maxStackSize = GetGame().ConfigGetInt( CFG_VEHICLESPATH + " " + classname + " varQuantityMax" );
+		}
+		else if (GetGame().ConfigIsExisting(CFG_MAGAZINESPATH + " " + classname))
+		{
+			maxStackSize = GetGame().ConfigGetInt( CFG_MAGAZINESPATH + " " + classname + " count" );
+		}
+		else if (GetGame().ConfigIsExisting(CFG_WEAPONSPATH + " " + classname))
+		{
+			maxStackSize = GetGame().ConfigGetInt( CFG_WEAPONSPATH + " " + classname + " varQuantityMax" );
+		}
+		
+		if ( maxStackSize > 0 )
+		{
+			string stackedUnits = "";
+			if (GetGame().ConfigIsExisting(CFG_VEHICLESPATH + " " + classname))
+			{
+				stackedUnits = GetGame().ConfigGetTextOut( CFG_VEHICLESPATH + " " + classname + " stackedUnit" );
+			}
+			else if (GetGame().ConfigIsExisting(CFG_MAGAZINESPATH + " " + classname))
+			{
+				stackedUnits = GetGame().ConfigGetTextOut( CFG_MAGAZINESPATH + " " + classname + " stackedUnit" );
+			}
+			else if (GetGame().ConfigIsExisting(CFG_WEAPONSPATH + " " + classname))
+			{
+				stackedUnits = GetGame().ConfigGetTextOut( CFG_WEAPONSPATH + " " + classname + " stackedUnit" );
+			}
+			
+			float item_quantity;
+			float item_max_quantity;
+			if ( stackedUnits == "pc." )
+			{
+				item_quantity = quantity * maxStackSize;
+				item_max_quantity = maxQuantity * maxStackSize;
+				WidgetTrySetText( root_widget, "ItemSelectedCountWidget", FormatBuyQuantityStr(item_quantity) + "/" + FormatBuyQuantityStr(item_max_quantity) + " " + "#inv_inspect_pieces" );	
+			}
+			else if ( GetGame().IsKindOf(classname, "Ammunition_Base") )
+			{
+				item_quantity = quantity * maxStackSize;
+				item_max_quantity = maxQuantity * maxStackSize;
+				WidgetTrySetText( root_widget, "ItemSelectedCountWidget", FormatBuyQuantityStr(item_quantity) + "/" + FormatBuyQuantityStr(item_max_quantity) + " " + "#inv_inspect_pieces" );
+			}
+			else
+			{
+				WidgetTrySetText( root_widget, "ItemSelectedCountWidget", FormatBuyQuantityStr(quantity) + "/" + FormatBuyQuantityStr(maxQuantity) + " " + "#inv_inspect_pieces" );
+			}
+		}
+		else
+		{
+			WidgetTrySetText( root_widget, "ItemSelectedCountWidget", FormatBuyQuantityStr(quantity) + "/" + FormatBuyQuantityStr(maxQuantity) + " " + "#inv_inspect_pieces" );
+		}
+	}
+	
 	private void WidgetTrySetText(Widget root_widget, string widget_name, string text, int color = 0)
 	{
 		TextWidget widget = TextWidget.Cast( root_widget.FindAnyWidget(widget_name) );
@@ -662,4 +781,12 @@ class SybTraderMenu extends UIScriptedMenu
 		root_widget.GetSize(w, h);
 		widget.SetSize(Math.Max(1, w - diff), h);
 	}
+};
+
+class SybTraderMenu_BuyData
+{
+	string m_classname;
+	float m_totalQuantity;
+	float m_selectedQuantity;
+	float m_maxBuyQuantity;
 };
