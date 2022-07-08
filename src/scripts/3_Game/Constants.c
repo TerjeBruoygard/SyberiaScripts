51696693b27cc5141316e6fb89bd1e2f6acfba25
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:37:39 +0400

    Fix zones and add additional gas colors

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,6 +1,6 @@
 const string Syberia_ModPreffix = "[SYBERIA] ";
 const bool Syberia_DebugMode = false;
-const string Syberia_Version = "OPEN BETA 0.47d";
+const string Syberia_Version = "OPEN BETA 0.47f";
 
 void SybLog(string message)
 {
--- src/scripts/3_Game/ParticleList.c
+++ src/scripts/3_Game/ParticleList.c
@@ -1,4 +1,10 @@
 modded class ParticleList
 {
-	static const int SYBERIA_SMOKE_TOXIC_PARTICLE = RegisterParticle( "SyberiaScripts/particles/" , "smoke_toxic_green");
+	static const int SYBERIA_SMOKE_TOXIC_PARTICLE_GREEN  = RegisterParticle( "SyberiaScripts/particles/" , "smoke_toxic_green");
+	static const int SYBERIA_SMOKE_TOXIC_PARTICLE_BLACK  = RegisterParticle( "SyberiaScripts/particles/" , "smoke_toxic_black");
+	static const int SYBERIA_SMOKE_TOXIC_PARTICLE_WHITE  = RegisterParticle( "SyberiaScripts/particles/" , "smoke_toxic_white");
+	static const int SYBERIA_SMOKE_TOXIC_PARTICLE_GREY   = RegisterParticle( "SyberiaScripts/particles/" , "smoke_toxic_grey");
+	static const int SYBERIA_SMOKE_TOXIC_PARTICLE_BLUE   = RegisterParticle( "SyberiaScripts/particles/" , "smoke_toxic_blue");
+	static const int SYBERIA_SMOKE_TOXIC_PARTICLE_RED    = RegisterParticle( "SyberiaScripts/particles/" , "smoke_toxic_red");
+	static const int SYBERIA_SMOKE_TOXIC_PARTICLE_YELLOW = RegisterParticle( "SyberiaScripts/particles/" , "smoke_toxic_yellow");
 }
