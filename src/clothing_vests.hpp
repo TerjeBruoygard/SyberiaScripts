269bfa8fe6524e709d7efc57da4203f23ca2e03c
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:38:49 +0400

    Finish with barter

--- src/clothing_vests.hpp
+++ src/clothing_vests.hpp
@@ -11,3 +11,14 @@ class PoliceVest: Clothing
 {
     bulletProofProtection = 0.1;
 };
+
+class TraderVest: PressVest_ColorBase
+{
+    visibilityModifier=0.7;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\vests\pressvest_trader_co.paa",
+        "SyberiaScripts\data\vests\pressvest_trader_co.paa",
+        "SyberiaScripts\data\vests\pressvest_trader_co.paa"
+    };
+};
