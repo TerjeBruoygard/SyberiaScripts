2de89289190eb43a9983e1eec06233076c41e73c
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:36:58 +0400

    Fix bugs

---
+++ src/scripts/4_World/Classes/UserActionsComponent/Actions/Continuous/SyberiaActionForceFeed.c
@@ -0,0 +1,21 @@
+modded class ActionForceFeed
+{
+	override bool ActionBlockerCondition( PlayerBase player, ActionTarget target, ItemBase item )
+	{
+		PlayerBase targetPlayer = PlayerBase.Cast(target.GetObject());
+		return (targetPlayer && targetPlayer.IsFaceBlocked(true)) || (item && item.IsTemperatureVisible() && item.GetTemperature() < 0);
+	}
+	
+	override string ActionBlockerText( PlayerBase player, ActionTarget target, ItemBase item )
+	{
+		if (item && item.IsTemperatureVisible() && item.GetTemperature() < 0)
+			return "#syb_action_consume_blocked_temp";
+		
+		return "#syb_action_forceconsume_blocked_headgear";
+	}
+	
+	override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
+	{
+		return CheckBlockerActionCondition(player, target, item) && super.ActionCondition(player, target, item);
+	}
+}
