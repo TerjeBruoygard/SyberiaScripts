3f4a0741de66bdda07165bbe2bcbb2ec08741c35
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:38:50 +0400

    Delete trader vest

--- src/clothing_vests.hpp
+++ src/clothing_vests.hpp
@@ -11,14 +11,3 @@ class PoliceVest: Clothing
 {
     bulletProofProtection = 0.1;
 };
-
-class TraderVest: PressVest_ColorBase
-{
-    visibilityModifier=0.7;
-    hiddenSelectionsTextures[]=
-    {
-        "SyberiaScripts\data\vests\pressvest_trader_co.paa",
-        "SyberiaScripts\data\vests\pressvest_trader_co.paa",
-        "SyberiaScripts\data\vests\pressvest_trader_co.paa"
-    };
-};
