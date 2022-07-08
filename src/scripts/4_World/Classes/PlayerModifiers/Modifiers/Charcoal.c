c9a8b356d9202f4f32e5d444b346407a01f27f1d
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:35:39 +0400

    Stomatchheal and Stomatchpoison

---
+++ src/scripts/4_World/Classes/PlayerModifiers/Modifiers/Charcoal.c
@@ -0,0 +1,27 @@
+modded class CharcoalMdfr
+{
+	override protected bool ActivateCondition(PlayerBase player)
+	{
+		return false;
+	}
+	
+	override protected bool DeactivateCondition(PlayerBase player)
+	{
+		return false;
+	}
+	
+	override protected void OnActivate(PlayerBase player)
+	{
+
+	}
+
+	override protected void OnDeactivate(PlayerBase player)
+	{
+
+	}
+	
+	override protected void OnTick(PlayerBase player, float deltaT)
+	{
+		
+	}
+};
