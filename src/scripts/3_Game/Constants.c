13ba2b9b4f496b7682be0b498b88a7197c4998bf
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:38:11 +0400

    Next door level

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,5 +1,5 @@
 const string Syberia_ModPreffix = "[SYBERIA] ";
-const bool Syberia_DebugMode = true;
+const bool Syberia_DebugMode = false;
 const string Syberia_Version = "OPEN BETA 0.48d";
 
 void SybLog(string message)
