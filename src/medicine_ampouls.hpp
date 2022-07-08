616bb935b43e7e5e68233d105e346838a0c49c2f
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:34:15 +0400

    Add moooooore medicine

--- src/medicine_ampouls.hpp
+++ src/medicine_ampouls.hpp
@@ -42,6 +42,62 @@ class AmpouleAlcohol : SyberiaMedicineAmpoule
     AMPOUL_ANIM_EVENT();
 };
 
+class AmpouleHihlofos : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_dihlofos_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medHemologicShock=1;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_red_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleCypermetrin : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_cypermetrin_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medHemologicShock=1;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_green_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleDiazinon : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_diazinon_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medHemologicShock=1;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_green_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleArsenic : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_arsenic_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medHemologicShock=1;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_green_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
 class AmpouleNovocain : SyberiaMedicineAmpoule
 {
     scope=2;
@@ -123,7 +179,7 @@ class AmpouleTiopental : SyberiaMedicineAmpoule
     displayName="#syb_tiopental_name";
     model="SyberiaScripts\data\medical\ampoule.p3d";
     medPainkillerLevel=3;
-    medPainkillerTimeSec=1100;
+    medPainkillerTimeSec=600;
     hiddenSelectionsTextures[]=
     {
         "SyberiaScripts\data\medical\ampoule_red_co.paa"
@@ -138,10 +194,284 @@ class AmpouleKetamin : SyberiaMedicineAmpoule
     displayName="#syb_ketamin_name";
     model="SyberiaScripts\data\medical\ampoule.p3d";
     medPainkillerLevel=3;
-    medPainkillerTimeSec=1400;
+    medPainkillerTimeSec=300;
     hiddenSelectionsTextures[]=
     {
         "SyberiaScripts\data\medical\ampoule_blue_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpoulePropanidid : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_propanidid_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medPainkillerLevel=2;
+    medPainkillerTimeSec=400;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_yellow_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleGexobarbital : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_gexobarbital_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medPainkillerLevel=2;
+    medPainkillerTimeSec=800;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleSodiumOxybutirate : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_sodium_oxybutirate_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medPainkillerLevel=3;
+    medPainkillerTimeSec=1200;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+
+
+
+
+class AmpouleMetoclopramid : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_metoclopramid_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medStomatchhealLevel=2;
+    medStomatchhealTimeSec=600;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_yellow_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleZerukal : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_zerukal_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medStomatchhealLevel=2;
+    medStomatchhealTimeSec=800;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_blue_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleKetorol : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_ketorol_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medStomatchhealLevel=2;
+    medStomatchhealTimeSec=1000;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_green_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleDiklofenak : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_diklofenak_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medStomatchhealLevel=2;
+    medStomatchhealTimeSec=1000;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleHeptral : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_heptral_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medStomatchhealLevel=2;
+    medStomatchhealTimeSec=1200;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+
+
+
+class AmpouleCylaspen : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_cylaspen_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medAntibioticLevel=2;
+    medAntibioticsTimeSec=600;
+    medAntibioticsStrength=2.0;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleGrimipenem : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_grimipenem_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medAntibioticLevel=2;
+    medAntibioticsTimeSec=500;
+    medAntibioticsStrength=1.8;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleZeftriaxon : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_zeftriaxon_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medAntibioticLevel=2;
+    medAntibioticsTimeSec=600;
+    medAntibioticsStrength=1.8;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_blue_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleErytromicyn : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_erytromicyn_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medAntibioticLevel=2;
+    medAntibioticsTimeSec=1000;
+    medAntibioticsStrength=2.0;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_red_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleFlemoclav : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_flemoclav_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medAntibioticLevel=2;
+    medAntibioticsTimeSec=1400;
+    medAntibioticsStrength=2.5;
+    medRemoveSepsis=1;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_yellow_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleOcsacylin : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_ocsacylin_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medAntibioticLevel=2;
+    medAntibioticsTimeSec=300;
+    medAntibioticsStrength=2.7;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_blue_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleImipenem : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_imipenem_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medAntibioticLevel=2;
+    medAntibioticsTimeSec=1200;
+    medAntibioticsStrength=3.0;
+    medRemoveSepsis=1;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleAmoksivan : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_amoksivan_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medAntibioticLevel=2;
+    medAntibioticsTimeSec=1400;
+    medAntibioticsStrength=3.0;
+    medRemoveSepsis=1;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_red_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+
+
+class AmpouleZivirol : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_zvirus_antidot_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medRemoveZVirus=1;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_red_co.paa"
     };
     ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
     AMPOUL_ANIM_EVENT();
@@ -160,6 +490,36 @@ class AmpouleMeksidol : SyberiaMedicineAmpoule
     ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
     AMPOUL_ANIM_EVENT();
 };
+
+class AmpouleActovegin : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_actovegin_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medConcussionHeal=1;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_blue_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleNeiroks : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_neiroks_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medConcussionHeal=1;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+
 
 class AmpoulePeklitakcel : SyberiaMedicineAmpoule
 {
@@ -190,16 +550,93 @@ class AmpouleNonacogAlpha : SyberiaMedicineAmpoule
     AMPOUL_ANIM_EVENT();
 };
 
+class AmpouleAminokaprolSulfat : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_aminokapsulfat_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medBloodHemostatic=1;
+    medBloodHemostaticTimeSec=600;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_red_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleVikasol : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_vikasol_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medBloodHemostatic=1;
+    medBloodHemostaticTimeSec=800;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_blue_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleEtamzilat : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_etamzilat_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medBloodHemostatic=1;
+    medBloodHemostaticTimeSec=1400;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_yellow_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
 class AmpoulePerftoran : SyberiaMedicineAmpoule
 {
     scope=2;
     displayName="#syb_perftoran_name";
     model="SyberiaScripts\data\medical\ampoule.p3d";
     medBloodHematopoiesis=1;
-    medBloodHematopoiesisTimeSec=200;
+    medBloodHematopoiesisTimeSec=300;
     hiddenSelectionsTextures[]=
     {
         "SyberiaScripts\data\medical\ampoule_blue_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleMaltofer : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_maltofer_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medBloodHematopoiesis=1;
+    medBloodHematopoiesisTimeSec=600;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_co.paa"
+    };
+    ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
+    AMPOUL_ANIM_EVENT();
+};
+
+class AmpouleErytropoenin : SyberiaMedicineAmpoule
+{
+    scope=2;
+    displayName="#syb_erytropoenin_name";
+    model="SyberiaScripts\data\medical\ampoule.p3d";
+    medBloodHematopoiesis=1;
+    medBloodHematopoiesisTimeSec=900;
+    medBloodHemostatic=1;
+    medBloodHemostaticTimeSec=900;
+    hiddenSelectionsTextures[]=
+    {
+        "SyberiaScripts\data\medical\ampoule_red_co.paa"
     };
     ITEM_DAMAGE_SYSTEM(50, "SyberiaScripts\data\medical\ampoule");
     AMPOUL_ANIM_EVENT();
