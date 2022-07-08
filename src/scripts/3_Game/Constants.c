3642e793681ece003b8a3930276963f2a1df8869
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:38:57 +0400

    Update version

--- src/scripts/3_Game/Constants.c
+++ src/scripts/3_Game/Constants.c
@@ -1,6 +1,6 @@
 const string Syberia_ModPreffix = "[SYBERIA] ";
 const bool Syberia_DebugMode = false;
-const string Syberia_Version = "OPEN BETA 0.48j";
+const string Syberia_Version = "OPEN BETA 0.48k";
 
 void SybLog(string message)
 {
