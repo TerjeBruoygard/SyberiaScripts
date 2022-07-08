9cb4032625e4e7ecb8560c418a12276652b341e3
Terje Bruoygard <ivankishchenko@protonmail.com>
2022-07-08 14:39:56 +0400

    Improve doors management for base building

--- src/scripts/4_World/Entities/Building/BuildingLivespace.c
+++ src/scripts/4_World/Entities/Building/BuildingLivespace.c
@@ -3,14 +3,11 @@ class BuildingLivespace extends BuildingSuper
 	// Local
 	protected bool m_ready = false;
 	protected ref LivespaceData m_data = null;
+	protected ref array<string> m_simpleSelections = new array<string>;
 	
 	// RPC Synch
-	protected int m_livespaceId;
-	protected string m_livespacePath;
-	protected int m_houseNetId1, m_houseNetId2;
-	protected ref array<int> m_doorLevels = new array<int>;
-	protected ref array<int> m_barricadeLevels = new array<int>;
-	protected ref array<string> m_simpleSelections = new array<string>;
+	ref LivespaceSynchData m_synchData = new LivespaceSynchData;
+
 	
 	static BuildingLivespace FindByVanillaDoorIndex(House house, int doorIndex)
 	{
@@ -52,51 +49,6 @@ class BuildingLivespace extends BuildingSuper
 		return result;
 	}
 	
-	static BuildingLivespace Find(House house, vector pos)
-	{
-		BuildingLivespace result = null;
-		BuildingLivespace iter = null;
-		vector localPos = house.WorldToModel(pos);
-		vector bbMin, bbMax, offset;
-		ref array<Object> objects = new array<Object>;
-		string configPath = "CfgBuildingInfo " + house.GetType();
-		int livespaceId = 0;
-		
-		while ( result == null && GetGame().ConfigIsExisting(configPath + " Livespace" + livespaceId) )
-		{
-			offset = GetGame().ConfigGetVector(configPath + " Livespace" + livespaceId + " offset");
-			offset = house.ModelToWorld(offset);
-			
-			objects.Clear();
-			GetGame().GetObjectsAtPosition3D(offset, 0.1, objects, NULL);
-			
-			foreach (Object obj : objects)
-			{
-				iter = BuildingLivespace.Cast(obj);
-				if (iter)
-				{
-					if (iter.m_ready && iter.GetHouse() == house)
-					{
-						if (GameHelpers.IntersectBBox(iter.m_data.m_bboxStart, iter.m_data.m_bboxEnd, localPos))
-						{
-							result = iter;
-							break;
-						}
-						else
-						{
-							SybLog("WARNING! LIVESPACE POINT MISSING INTERSECTION WITH BBOX " + house.GetType());
-						}
-					}
-				}
-			}
-			
-			livespaceId = livespaceId + 1;
-		}
-		
-		delete objects;
-		return result;
-	}
-	
 	override void EEInit()
 	{
 		super.EEInit();
@@ -115,23 +67,17 @@ class BuildingLivespace extends BuildingSuper
 		
 		if (rpc_type == SyberiaERPC.SYBERPC_SYNCH_LIVESPACE_SERVER)
 		{
-			Param6<string, int, int, int, ref array<int>, ref array<int>> params;
+			Param1<ref LivespaceSynchData> params;
 			if (!ctx.Read(params))
 				return;
 			
-			if (m_doorLevels) delete m_doorLevels;
-			if (m_barricadeLevels) delete m_barricadeLevels;
+			if (m_synchData) delete m_synchData;
 			
-			m_livespacePath = params.param1;
-			m_livespaceId = params.param2;
-			m_houseNetId1 = params.param3;
-			m_houseNetId2 = params.param4;
-			m_doorLevels = params.param5;
-			m_barricadeLevels = params.param6;
-			
+			m_synchData = params.param1;
+
 			if (m_data == null)
 			{
-				m_data = new LivespaceData(m_livespacePath);
+				m_data = new LivespaceData(m_synchData.m_livespacePath);
 			}
 			
 			int marcSelectionId = FindSimpleHidenSelectionId("marc");
@@ -156,12 +102,17 @@ class BuildingLivespace extends BuildingSuper
 	House GetHouse()
 	{
 		if (!m_ready) return null;
-		return House.Cast( GetGame().GetObjectByNetworkId(m_houseNetId1, m_houseNetId2) );
+		return House.Cast( GetGame().GetObjectByNetworkId(m_synchData.m_houseNetId1, m_synchData.m_houseNetId2) );
+	}
+	
+	bool IsReady()
+	{
+		return m_ready;
 	}
 	
 	int GetLivespaceId()
 	{
-		return m_livespaceId;
+		return m_synchData.m_livespaceId;
 	}
 	
 	int FindSimpleHidenSelectionId(string name)
@@ -174,7 +125,7 @@ class BuildingLivespace extends BuildingSuper
 		for (int i = 0; i < m_data.m_doors.Count(); i++) 
 		{
 			ref LivespaceDoorData doorData = m_data.m_doors[i];
-			int level = m_doorLevels[i] - 1;
+			int level = m_synchData.m_doorLevels[i] - 1;
 			for (int l = 0; l < doorData.m_levels.Count(); l++)
 			{
 				int selectionId = FindSimpleHidenSelectionId(doorData.m_levels[l]);
@@ -188,10 +139,10 @@ class BuildingLivespace extends BuildingSuper
 	
 	void SetupBarricades()
 	{
-		for (int i = 0; i < m_barricadeLevels.Count(); i++) 
+		for (int i = 0; i < m_synchData.m_barricadeLevels.Count(); i++) 
 		{
 			ref LivespaceBarricadeData barricadeData = m_data.m_barricades[i];
-			int level = m_barricadeLevels[i] - 1;
+			int level = m_synchData.m_barricadeLevels[i] - 1;
 			for (int l = 0; l < barricadeData.m_levels.Count(); l++)
 			{
 				int selectionId = FindSimpleHidenSelectionId(barricadeData.m_levels[l]);
@@ -209,7 +160,7 @@ class BuildingLivespace extends BuildingSuper
 		int doorId = GetDoorIdByVanillaDoorId(vanilaDoorIndex);
 		if (doorId == -1) return true;
 		
-		int level = m_doorLevels[doorId];		
+		int level = m_synchData.m_doorLevels[doorId];		
 		return level == 0;
 	}
 	
@@ -239,14 +190,35 @@ class BuildingLivespace extends BuildingSuper
 	{
 		super.EEDelete(parent);
 		
-		delete m_doorLevels;
-		delete m_barricadeLevels;
-		delete m_simpleSelections;
+		if (m_synchData) 
+		{
+			delete m_synchData;
+		}
+		
+		if (m_simpleSelections) 
+		{
+			delete m_simpleSelections;
+		}
 		
 		if (m_data != null)
 		{
 			delete m_data;
 		}
+	}
+};
+
+class LivespaceSynchData
+{
+	int m_livespaceId;
+	string m_livespacePath;
+	int m_houseNetId1, m_houseNetId2;
+	ref array<int> m_doorLevels = new array<int>;
+	ref array<int> m_barricadeLevels = new array<int>;
+	
+	void ~LivespaceSynchData()
+	{
+		if (m_doorLevels) delete m_doorLevels;
+		if (m_barricadeLevels) delete m_barricadeLevels;
 	}
 };
 
